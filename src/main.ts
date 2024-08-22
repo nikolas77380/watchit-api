6f7d1e89d14223109acd94b1626badb16b643c77
nikolas77380 <mykola.kipniak@fishbrain.com>
2024-08-22 11:15:10 +0300

    enable swagger

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,16 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
   });
-  await app.listen(3008);
 
-  const options = new DocumentBuilder()
-    .setTitle('API')
-    .setDescription('API description')
+  const config = new DocumentBuilder()
+    .setTitle('Watchit api')
+    .setDescription('Watchit API dodcumentation')
     .setVersion('1.0')
-    .addTag('API')
+    .addTag('Swagger')
     .build();
-  const document = SwaggerModule.createDocument(app, options, {
-    // include: [UserModule, ArticleModule],
-  });
-
+  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  await app.listen(3008);
 }
 bootstrap();
